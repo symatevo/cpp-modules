cd86b94543e73c9f5b5cfeefdb786b8a176d24c2
Syuzanna Matevosyan <74954267+symatevo@users.noreply.github.com>
2022-11-04 20:34:13 +0400

    Add files via upload

--- cpp04/ex01/main.cpp
+++ cpp04/ex01/main.cpp
@@ -4,26 +4,30 @@
 
 int main()
 {
-    const Animal* k = new Dog();
-    const Animal* b = new Cat();
-    delete k;//should not create a leak
-    delete b;
-    const Animal *d[100];
-    for (int i = 0; i < 50; i++) {
-        d[i] = new Dog();
-    }
+    // const Animal* k = new Dog();
+    // const Animal* b = new Cat();
+    // delete k;//should not create a leak
+    // delete b;
+    // const Animal *d[100];
+    // for (int i = 0; i < 50; i++) {
+    //     d[i] = new Dog();
+    // }
     
-    for (int i = 50; i < 100; i++) {
-        d[i] = new Cat();
-    }
+    // for (int i = 50; i < 100; i++) {
+    //     d[i] = new Cat();
+    // }
 
-    for (int i = 0; i < 50; i++) {
-        delete d[i];
-    }
+    // for (int i = 0; i < 50; i++) {
+    //     delete d[i];
+    // }
     
-    for (int i = 50; i < 100; i++) {
-        delete d[i];
+    // for (int i = 50; i < 100; i++) {
+    //     delete d[i];
+    // }
+    Dog basic;
+    {
+        Dog tmp = basic;
     }
-
+    std::cout << "ok\n";
     return 0;
 }
